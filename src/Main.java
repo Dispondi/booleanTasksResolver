084ca55f474a2fd3a0670b39acdf0031cd6a9af0
Andrew Grachev <andrey.grachev.2007@mail.ru>
2025-03-05 02:29:25 +0300

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        HashMap<Integer, Boolean> inputBoolean = new HashMap<>() {{ // вводить данные сюда
+            put(0, true);
+            put(1, true);
+            put(2, null);
+            put(3, true);
+            put(4, false);
+            put(5, false);
+            put(6, true);
+            put(7, false);
+            put(8, null);
+            put(9, true);
+            put(10, false);
+            put(11, false);
+            put(12, null);
+            put(13, null);
+            put(14, true);
+            put(15, null);
+        }};
+
         boolean[][] inputArray = new boolean[16][4];
         for (int i = 0; i < 16; i++) {
             String binaryDigit = Integer.toBinaryString(i);
@@ -24,8 +47,15 @@ public class Main {
             }
             System.out.print(" ");
             boolean resolve = booleanTask(inputArray[i]);
-            if (resolve) System.out.println(1);
-            else System.out.println(0);
+            String outputRow = "";
+            if (resolve) outputRow += 1;
+            else outputRow += 0;
+            if (inputBoolean.get(i) == null) {
+                System.out.println(outputRow);
+                continue;
+            }
+            outputRow += inputBoolean.get(i) == resolve ? " Совпадает" : " Не совпадает";
+            System.out.println(outputRow);
         }
     }
     public static boolean stringToBool(String s) {
@@ -36,7 +66,7 @@ public class Main {
         throw new IllegalArgumentException(s+" is not a bool. Only 1 and 0 are.");
     }
 
-    public static boolean booleanTask(boolean[] arr) {
-        return arr[3] && arr[2] || !arr[0] && arr[1] || !arr[1] && !arr[2] || arr[0] && !arr[2];
+    public static boolean booleanTask(boolean[] arr) { // вводить функцию сюда
+        return (arr[1] || !arr[2]) && (!arr[0] || !arr[2]) && (!arr[1] || arr[2] || !arr[3]);
     }
 }
